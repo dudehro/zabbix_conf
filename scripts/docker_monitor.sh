72e1ccc6f70f81dbbc5681b01c6218ca4ba27c3b
Peter Korduan <peter.korduan@gdi-service.de>
2021-04-06 13:56:09 +0200

    new UserParameter gdi.docker.pgsql_versions

--- scripts/docker_monitor.sh
+++ scripts/docker_monitor.sh
@@ -16,8 +16,37 @@ function get_postgis_image_tags(){
 	done
 }
 
+function docker_ps_pgsql_container(){
+	while read TAG
+	do
+		DOCKERPS=$(docker ps -a --format "{{.Image}}\t{{.Names}}\t{{.Status}}\t{{.Networks}}" --filter ancestor=pkorduan/postgis:$TAG)
+		if [[ -n $DOCKERPS ]]; then
+			echo $DOCKERPS
+		fi
+	done < <(get_postgis_image_tags)
+}
+
+function get_pgsql_versions(){
+        while read TAG
+        do
+                DOCKERPS=$(docker ps -a --format "{{.Names}}" --filter ancestor=pkorduan/postgis:$TAG)
+                if [[ -n $DOCKERPS ]]; then
+                        echo $TAG
+                fi
+        done < <(get_postgis_image_tags)
+}
+
 case $1 in
-	list_pgsql_container)
+	list-postgis-image-tags)
+		get_postgis_image_tags
+	;;
+	list-pgsql-container-ids)
 		get_pgsql_container
+	;;
+	docker-ps-pgsql)
+		docker_ps_pgsql_container
+	;;
+	list-pgsql-versions)
+		get_pgsql_versions
 	;;
 esac
