39d3049edd1b924316c483f4b7b8272907994698
Peter Korduan <peter.korduan@gdi-service.de>
2022-06-10 10:17:16 +0200

    changed cut with awk

--- scripts/kvwmap_git_branch.sh
+++ scripts/kvwmap_git_branch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git_directory="/home/gisadmin/kvwmap-server"
 if [ -d "$git_directory" ]; then
-  cd ${git_directory} && git status | grep -i on | cut -d ' ' -f 3
+  cd ${git_directory} && git status | grep On | awk '{print $3}'
+#cut -d ' ' -f 3
 else
    echo "'${git_directory}' directory is NOT exist"
 fi
