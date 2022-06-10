09c7bceb43fbb8a80e3e083e7422f0e04216a6d9
Peter Korduan <peter.korduan@gdi-service.de>
2022-06-10 08:55:35 +0000

    Changed git status parameter

--- scripts/kvwmap_git_branch.sh
+++ scripts/kvwmap_git_branch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 git_directory="/home/gisadmin/kvwmap-server"
+
 if [ -d "$git_directory" ]; then
-  cd ${git_directory} && git status | grep On | awk '{print $3}'
-#cut -d ' ' -f 3
+  cd ${git_directory}; git rev-parse --abbrev-ref HEAD
 else
    echo "'${git_directory}' directory is NOT exist"
 fi
